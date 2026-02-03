fcbd688effbfb23f748d2a95dc671fe6d35a0277
Jaime Lopez <35352512+MrTwoMile@users.noreply.github.com>
2026-02-03 00:11:01 -0500

    Turn off upper custom

--- mp4tag.go
+++ mp4tag.go
@@ -67,7 +67,7 @@ func Open(trackPath string) (*MP4, error) {
 		f: f,
 		size : stat.Size(),
 		path: trackPath,
-		upperCustom: true,
+		upperCustom: false,
 	}
 	err = mp4.checkHeader()
 	if err != nil {
@@ -75,4 +75,5 @@ func Open(trackPath string) (*MP4, error) {
 		return nil, err
 	}
 	return mp4, nil
+
 }
